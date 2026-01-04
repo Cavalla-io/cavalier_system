1843d8f6ca638b0e04a776fe62381771974abdda
Victor <victor.w.boyd@gmail.com>
2026-01-04 12:15:42 -0800

    init

---
+++ system/forklift_start.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+import json
+from pathlib import Path
+#import curtis_f2a_fault_codes.json
+
+def check_can():
+    can_check_command = ["sudo", "ip", "link", "show", "can0"]
+    result = subprocess.run(can_check_command, capture_output=True, text=True, check=True)
+    if "UP" in result.stdout:
+        final_result = True
+    else:
+        final_result = False
+    return final_result
+
+def setup_can():
+    subprocess.run(["sudo", "ip", "link", "set", "up", "can0", "type", "can", "bitrate", "250000"])
+
+def check_ros():
+    check_topic_command = ["ros2","topic","list"]
+    check_node_command = ["ros2","node","list"]
+    topic_result = subprocess.run(check_topic_command, capture_output=True, text=True, check=True)
+    node_result = subprocess.run(check_node_command, capture_output=True, text=True, check=True)
+
+def check_docker():
+    check_docker_command = ["sudo","docker","ps","-a","--no-trunc"]
+    result = subprocess.run(check_docker_command, capture_output=True, text=True, check=True)
+
+def check_drive_fault():
+    check_fault_command = ["candump","can0,183:7ff","-n","1"]
+    result = subprocess.run(check_fault_command, capture_output=True, text=True, check=True)
+    result_stripped = result.stdout.strip()
+    result_array = result_stripped.split()
+    fault_byte = list(result_array[7])
+    fault_code = fault_byte[0] + "-" + fault_byte[1]
+
+    json_code = json.loads(Path('curtis_f2a_fault_codes.json').read_text())
+    fault_description = json_code["flash_code"][fault_code]
+
+    return fault_description
+
+print(check_drive_fault())
---
+++ system/ros_system/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'ros_system'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cavallatestbench',
+    maintainer_email='victor.w.boyd@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'forklift_health = ros_system.forklift_health:main'
+        ],
+    },
+)
